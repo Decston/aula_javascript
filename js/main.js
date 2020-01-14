012ebdeb25f37bbab173255eed95783d273ac2e9
ALISSON PATRICIO GOMES DE FREITAS <ALISSON.PATRICIO@SME.FORTALEZA.CE.GOV.BR>
2020-01-14 16:13:42 -0300

    Mudança de algumas funções e inserção de comentários no código e de funcionamento.

---
+++ js/main.js
@@ -0,0 +1,15 @@
+
+var nome = "Alisson Patrício";
+var idade = 25;
+var idade2 = 10;
+var frase = "Japão é a melhor seleção do mundo!";
+var palavra = "prensa mais";
+/*alert(nome+" tem "+idade+" anos!");
+//alert(idade + idade2);
+alert(frase);
+//console.log(nome);
+//console.log(idade);
+console.log(frase.replace("Japão", "Brasil"));
+console.log(nome.toUpperCase());
+console.log(frase.toLowerCase());*/
+console.log(palavra.replace("prensa", "Pensa"));
